52e270e30ab4855c96e20721bcebf7afd536fa9c
fenny.ren <fenny.ren@cootek.cn>
2019-06-08 00:12:56 +0800

    重构

--- src/page/BasePage.py
+++ src/page/BasePage.py
@@ -66,49 +66,49 @@ class BasePage:
     def load_steps(self, path, key, **kwargs):
         with open(path, 'r') as f:
             po_data = yaml.load(f.read())
-            po_method = po_data[key]
-            if 'elements' in po_data:
-                po_elements = po_data['elements']
-            for step in po_method:
-                logging.info(step)
-                if 'element' in step:
-                    element_dict = po_elements[step['element']][Client.platform]
+        po_method = po_data[key]
+        if 'elements' in po_data:
+            po_elements = po_data['elements']
+        for step in po_method:
+            logging.info(step)
+            if 'element' in step:
+                element_dict = po_elements[step['element']][Client.platform]
+            else:
+                element_dict = {'by': step['by'], 'locator': step['locator']}
+            tips = step['tips'] if 'tips' in step else '操作元素：{}'.format(element_dict['locator'])
+            if '$' in element_dict['locator']:
+                for k, v in kwargs.items():
+                    origin: str = element_dict['locator']
+                    element_dict['locator'] = origin.replace('$%s' % k, v)
+                    logging.info('origin: {}, new: {}'.format(origin, element_dict['locator']))
+                    if origin != element_dict['locator']:
+                        del kwargs[k]
+                    if '$' not in element_dict['locator']:
+                        break
+            logging.info('元素的定位符是：{}'.format(element_dict['locator']))
+            with allure.step(tips):
+                if element_dict['by'] == 'text':
+                    element: WebElement = self.find_by_text(element_dict['locator'])
                 else:
-                    element_dict = {'by': step['by'], 'locator': step['locator']}
-                tips = step['tips'] if 'tips' in step else '操作元素：{}'.format(element_dict['locator'])
-                if '$' in element_dict['locator']:
+                    element: WebElement = self.find(by=element_dict['by'], value=element_dict['locator'])
+                action = str(step['action']).lower()
+                if action == 'click':
+                    element.click()
+                elif action == 'send_keys':
                     for k, v in kwargs.items():
-                        origin: str = element_dict['locator']
-                        element_dict['locator'] = origin.replace('$%s' % k, v)
-                        logging.info('origin: {}, new: {}'.format(origin, element_dict['locator']))
-                        if origin != element_dict['locator']:
+                        origin: str = step['text']
+                        new = origin.replace('$%s' % k, v)
+                        logging.info('origin: {}, new: {}'.format(origin, new))
+                        if origin != new:
                             del kwargs[k]
-                        if '$' not in element_dict['locator']:
                             break
-                logging.info('元素的定位符是：{}'.format(element_dict['locator']))
-                with allure.step(tips):
-                    if element_dict['by'] == 'text':
-                        element: WebElement = self.find_by_text(element_dict['locator'])
-                    else:
-                        element: WebElement = self.find(by=element_dict['by'], value=element_dict['locator'])
-                    action = step['action']
-                    if action == 'click':
-                        element.click()
-                    elif action == 'send_keys':
-                        for k, v in kwargs.items():
-                            origin: str = step['text']
-                            new = origin.replace('$%s' % k, v)
-                            logging.info('origin: {}, new: {}'.format(origin, new))
-                            if origin != new:
-                                del kwargs[k]
-                                break
-                        element.send_keys(new)
-                    elif action == 'text':
-                        return element.text
-                    elif action == 'resourceId':
-                        return element.get_attribute('resourceId')
-                    else:
-                        logging.info('UNKNOW COMMAND %s' % step)
+                    element.send_keys(new)
+                elif action == 'text':
+                    return element.text
+                elif action == 'get_attribute':
+                    return element.get_attribute(step['attribute'])
+                else:
+                    logging.info('UNKNOW COMMAND %s' % step)
 
     def wait_element_displayed(self, kv, wait: int=10, message: str='') -> WebElement or bool:
         max_time = time.time() + wait
